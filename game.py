f82c6fce51fe1fcd874121a5416d10dfbaac923f
Tatianafortech <111741943+Tatianafortech@users.noreply.github.com>
2024-01-22 08:38:33 +0200

    Update game.py

--- game.py
+++ game.py
@@ -1,39 +1,38 @@
-print('Welcome to AskPython Quiz')
-answer=input('Are you ready to play the Quiz ? (yes/no) :')
-score=0
-total_questions=3
+# game.py
 
-if answer.lower()=='yes':
-    answer=input('Question 1: What is your Favourite programming language?')
-    if answer.lower()=='python':
-        score += 1
-        print('correct')
-    else:
-        print('Wrong Answer :(')
+def run_quiz(answer):
+    score = 0
+    total_questions = 3
 
+    print('Welcome to AskPython Quiz')
+    user_ready = input('Are you ready to play the Quiz? (yes/no): ')
 
-    answer=input('Question 2: Do you follow any author on AskPython? ')
-    if answer.lower()=='yes':
-        score += 1
-        print('correct')
-    else:
-        print('Wrong Answer :(')
+    if user_ready.lower() == 'yes':
+        answer_1 = input('Question 1: What is your favorite programming language? ')
+        if answer_1.lower() == 'python':
+            score += 1
+            print('Correct!')
+        else:
+            print('Wrong Answer :(')
 
-    answer=input('Question 3: What is the name of your favourite website for learning Python?')
-    if answer.lower()=='askpython':
-        score += 1
-        print('correct')
-    else:
-        print('Wrong Answer :(')
+        answer_2 = input('Question 2: Do you follow any author on AskPython? ')
+        if answer_2.lower() == 'yes':
+            score += 1
+            print('Correct!')
+        else:
+            print('Wrong Answer :(')
 
-print('Thank you for Playing this small quiz game, you attempted',score,"questions correctly!")
-mark=(score/total_questions)*100
-print('Marks obtained:',mark)
-print('BYE!')
+        answer_3 = input('Question 3: What is the name of your favorite website for learning Python? ')
+        if answer_3.lower() == 'askpython':
+            score += 1
+            print('Correct!')
+        else:
+            print('Wrong Answer :(')
 
-def run_tests():
-    assert run_quiz('python') == 1
-    assert run_quiz('no') == 0
+    print(f'Thank you for playing this small quiz game. You attempted {score} questions correctly!')
+    mark = (score / total_questions) * 100
+    print(f'Marks obtained: {mark}')
+    print('BYE!')
 
 if __name__ == '__main__':
-    run_tests()
+    run_quiz('python')  # You can replace this with user input if needed
