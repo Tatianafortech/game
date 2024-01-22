b81d75f8c7df0398ce994d87630f3344c91c0e46
Tatianafortech <111741943+Tatianafortech@users.noreply.github.com>
2024-01-22 08:53:08 +0200

    Update game.py

--- game.py
+++ game.py
@@ -6,30 +6,33 @@ def run_quiz(user_ready='yes', answer_1='python', answer_2='yes', answer_3='askp
 
     print('Welcome to AskPython Quiz')
     if user_ready.lower() == 'yes':
-        if input('Question 1: What is your favorite programming language? ').lower() == answer_1:
+        if ask_question('Question 1: What is your favorite programming language?', answer_1):
             score += 1
-            print('Correct!')
-        else:
-            print('Wrong Answer :(')
 
-        if input('Question 2: Do you follow any author on AskPython? ').lower() == answer_2:
+        if ask_question('Question 2: Do you follow any author on AskPython?', answer_2):
             score += 1
-            print('Correct!')
-        else:
-            print('Wrong Answer :(')
 
-        if input('Question 3: What is the name of your favorite website for learning Python? ').lower() == answer_3:
+        if ask_question('Question 3: What is the name of your favorite website for learning Python?', answer_3):
             score += 1
-            print('Correct!')
-        else:
-            print('Wrong Answer :(')
 
     print(f'Thank you for playing this small quiz game. You attempted {score} questions correctly!')
     mark = (score / total_questions) * 100
     print(f'Marks obtained: {mark}')
     print('BYE!')
 
-if __name__ == '__main__':
-    run_quiz()
+def ask_question(question, correct_answer):
+    user_answer = input(question).lower()
+    if user_answer == correct_answer:
+        print('Correct!')
+        return True
+    else:
+        print('Wrong Answer :(')
+        return False
+
+def run_tests():
+    # Run the quiz with correct answers for testing purposes
     run_quiz_correct(answer_1='python', answer_2='yes', answer_3='askpython')
 
+if __name__ == '__main__':
+    run_quiz()
+    run_tests()
