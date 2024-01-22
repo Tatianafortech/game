f368d48fe4faa37fd394df90b23ee6b97f8dc28c
Tatianafortech <111741943+Tatianafortech@users.noreply.github.com>
2024-01-22 21:12:12 +0200

    Update test_game.py

--- test_game.py
+++ test_game.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, call
 from game import run_quiz, get_user_input
 
 class TestGame(unittest.TestCase):
@@ -8,28 +8,30 @@ class TestGame(unittest.TestCase):
     def test_run_quiz_all_correct_answers(self, mock_print, mock_input):
         run_quiz()
         expected_calls = [
-            'Welcome to the Quiz!',
-            'Correct!', 'Correct!', 'Correct!',
-            'Thank you for playing. You got 3 out of 3 questions correct!',
-            'Marks obtained: 100.00%',
-            'BYE!'
+            call('Welcome to the Quiz!'),
+            call('Correct!'),
+            call('Correct!'),
+            call('Correct!'),
+            call('Thank you for playing. You got 3 out of 3 questions correct!'),
+            call('Marks obtained: 100.00%'),
+            call('BYE!')
         ]
-        mock_print.assert_has_calls([call(msg) for msg in expected_calls])
+        mock_print.assert_has_calls(expected_calls)
 
     @patch('builtins.input', side_effect=['london', 'no', '5'])
     @patch('builtins.print')
     def test_run_quiz_some_incorrect_answers(self, mock_print, mock_input):
         run_quiz()
         expected_calls = [
-            'Welcome to the Quiz!',
-            "Wrong Answer. The correct answer is paris.",
-            "Wrong Answer. The correct answer is yes.",
-            "Wrong Answer. The correct answer is 7.",
-            'Thank you for playing. You got 0 out of 3 questions correct!',
-            'Marks obtained: 0.00%',
-            'BYE!'
+            call('Welcome to the Quiz!'),
+            call("Wrong Answer. The correct answer is paris."),
+            call("Wrong Answer. The correct answer is yes."),
+            call("Wrong Answer. The correct answer is 7."),
+            call('Thank you for playing. You got 0 out of 3 questions correct!'),
+            call('Marks obtained: 0.00%'),
+            call('BYE!')
         ]
-        mock_print.assert_has_calls([call(msg) for msg in expected_calls])
+        mock_print.assert_has_calls(expected_calls)
 
     @patch('builtins.input', return_value='user_input')
     def test_get_user_input(self, mock_input):
